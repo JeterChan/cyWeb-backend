5085ff4f545a755797fde27842e0c495027e9810
Jeter Chan <clocklife0226@gmail.com>
2024-10-09 00:06:09 +0800

    feat:send email api

---
+++ cyWeb.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const cors = require("cors");
+
+// router
+const productRouter = require('./routes/productRouter.js');
+const apiRouter = require('./routes/apiRouter.js');
+
+dotenv.config();
+
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use(
+    cors({
+        origin:"*",
+    })
+);
+
+const mongo = process.env.MONGODB;
+mongoose.connect(mongo,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+}) 
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("mongo connected");
+});
+
+// Set view engine to EJS
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.use('/product', productRouter);
+app.use('/api', apiRouter);
+
+app.listen(8080, () => {
+    console.log('Server listening on "http://localhost:8080"');
+});
