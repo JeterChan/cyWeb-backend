62bb6cbd66f1d4fe7bd8a3c37c1802bbc8f81473
JeterChan <clocklife0226@gmail.com>
2025-05-07 17:18:09 +0200

    feat:: debug authentication

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,5 @@
 const passport = require('passport');
-const LocalStategy = require('passport-local').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
 const { User } = require('../db/models');
 const bcrypt = require('bcrypt');
 
@@ -9,7 +9,7 @@ module.exports = app => {
     app.use(passport.initialize());
     app.use(passport.session());
     // 設定本地登入策略
-    passport.use(new LocalStategy({ usernameField: 'email'}, async (email, password, done) => {
+    passport.use(new LocalStrategy({ usernameField: 'email'}, async (email, password, done) => {
         try {
             const user = await User.findOne({
                 where: {email: email}
@@ -21,9 +21,8 @@ module.exports = app => {
                     message:'這個信箱沒有被註冊'
                 })
             }
-
             // 比較 password
-            const isMatch = bcrypt.compare(password, user.password)
+            const isMatch = await bcrypt.compare(password, user.password)
             // 若密碼不相等
             if(!isMatch) {
                 return done(null, false, {
