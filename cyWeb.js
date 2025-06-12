2cfaa05748e49898fc54340730c90289e1162f58
JeterChan <clocklife0226@gmail.com>
2025-06-12 15:57:39 +0800

    update .gitnore & .dockerignore

--- cyWeb.js
+++ cyWeb.js
@@ -30,15 +30,15 @@ const app = express();
 const setLocals = require('./middleware/locals');
 
 // use morgan for logging
-switch(process.env.NODE_ENV) {
-  case 'development':
-    app.use(morgan('dev'))
-    break
-  case 'production':
-    const stream = fs.createWriteStream(path.join(__dirname + 'access.log'),{flags:'a'});
-    app.use(morgan('combined', {stream}));
-    break  
-}
+// switch(process.env.NODE_ENV) {
+//   case 'development':
+//     app.use(morgan('dev'))
+//     break
+//   case 'production':
+//     const stream = fs.createWriteStream(path.join(__dirname + 'access.log'),{flags:'a'});
+//     app.use(morgan('combined', {stream}));
+//     break  
+// }
 
 // session config
 app.use(session)
