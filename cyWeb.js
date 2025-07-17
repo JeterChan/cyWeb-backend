bb47382c53d8a7d4db2427ed21d3f8ea5e9c5e2f
Jeter Chan <clocklife0226@gmail.com>
2025-07-17 21:57:44 +0800

    modify version error, and push the newest version

--- cyWeb.js
+++ cyWeb.js
@@ -9,15 +9,6 @@ const flash = require('connect-flash');
 const session = require('./middleware/session');
 const morgan = require('morgan');
 
-
-// swagger
-// development only
-if(process.env.NODE_ENV === 'development') {
-  const swaggerUi = require('swagger-ui-express');
-  const swaggerSpec = require('./swagger.js');
-}
-
-
 // router
 const productRouter = require('./routes/productRouter.js');
 const adminRouter = require('./routes/adminRouter.js');
@@ -56,7 +47,13 @@ app.set('layout','layouts/main');
 app.use(express.static('public'));
 
 // swagger
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+// development only
+if(process.env.NODE_ENV === 'development') {
+  const swaggerUi = require('swagger-ui-express');
+  const swaggerSpec = require('./swagger.js');
+  // swagger
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+}
 
 // router path
 app.get('/', (req, res) => {
@@ -82,7 +79,4 @@ app.use((req, res, next) => {
 });
 
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server listening on "http://localhost:${process.env.PORT}"`);
-    console.log(`Swagger API docs at "http://localhost:${process.env.PORT}/api-docs"`);
-});
+app.listen(process.env.PORT || 8080);
